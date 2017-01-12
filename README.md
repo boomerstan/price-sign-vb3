70afcebacd3bfc6cf99d995ace2fee649c1f9efb
Stan Peters <stanleypeters@gmail.com>
2017-01-11 21:48:44 -0700

    CONVERT PRICE-SIGN-VF TO BOOTSTRAP 3

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # price-sign-vb3
 Demo of front end UI for a price sign controller using Vue 2.1 and Bootstrap 3.3.7
+
+
+The target market was single stores or small chains marketing fuel.
+
+I originally created this UI using AngularJS 1.4 and Foundation for Sites 5. It was embedded on a Beaglebone Black processor using lighttpd as the web server and Laravel 4 as the backend API. There was a lot more code involved for managing the signs' features, but that was proprietary knowledge. This website was intranet, never public. 
+
+Vue is a much more appropriate JavaScript framework for this project than Angular. I intend to duplicate it using other CSS frameworks.
+
+[DEMO](https://boomerstan.github.io/price-sign-vb3/) 
+
+TODO: Remove unused styles from app.css.
---
+++ js/app.js
@@ -0,0 +1,168 @@
+var same  = "Price is same as on sign.",
+    sames = "Prices are same as on sign.",
+    diff  = "Price is different than on sign.",
+    diffs = "Prices are different than on sign.";
+
+new Vue({
+    el: 'main',
+
+    data: {
+        detail: false,
+        detailText: "Show Price Tiers",
+        signPrice: sames,
+
+        commodities: [
+            {
+                id: 0,
+                name: 'Unleaded',
+                price: 1.85,
+                priceTitle: same,
+                tierDiffs: [ 0, 0.05, 0.08, -0.03 ]
+            },
+            {
+                id: 1,
+                name: 'Midgrade',
+                price: 1.94,
+                priceTitle: same,
+                tierDiffs: [ 0, 0.05, 0.08, -0.03 ]
+            },
+            {
+                id: 2,
+                name: 'Premium',
+                price: 2.06,
+                priceTitle: same,
+                tierDiffs: [ 0, 0.05, 0.08, -0.03 ]
+            },
+            {
+                id: 3,
+                name: 'Flexfuel',
+                price: 1.55,
+                priceTitle: same,
+                tierDiffs: [ 0, 0.05, 0.08, -0.03 ]
+            },
+            {
+                id: 4,
+                name: 'Diesel',
+                price: 2.75,
+                priceTitle: same,
+                tierDiffs: [ 0, 0.05, 0.08, -0.03 ]
+            }],
+        tiers: [
+            {
+                id: 0,
+                name: "Cash Price",
+            },
+            {
+                id: 1,
+                name: "Debit Price",
+            },
+            {
+                id: 2,
+                name: "Credit Price",
+            },
+            {
+                id: 3,
+                name: "Loyalty Price",
+            }
+        ]
+    },
+
+    computed: {
+        signPriceState: function () {
+            return this.signPrice;
+        },
+        signState: function () {
+            return this.signPrice == diffs;
+        }
+    },
+
+    mounted () {
+        this.commodities.forEach(function(commodity) {
+            commodity.resetTierDiffs = [];
+            commodity.tierDiffsChanged = [];
+            commodity.reset = commodity.price;
+            commodity.priceChanged = false;
+            commodity.tierDiffs.forEach(function (value) {
+                commodity.resetTierDiffs.push(value);
+                commodity.tierDiffsChanged.push(false);
+            });
+        });
+    },
+
+    methods: {
+        toggleDetail: function () {
+            this.detail = ! this.detail;
+            this.detailText = this.detail ? "Hide Price Tiers" : "Show Price Tiers";
+        },
+        resetPrices: function () {
+            this.commodities.forEach(function (commodity){
+                commodity.price = commodity.reset;
+                commodity.resetTierDiffs.forEach(function (tier, index) {
+                    if(tier!=commodity.tierDiffs[index]){
+                        commodity.tierDiffs[index] = commodity.resetTierDiffs[index]
+                    }
+                })
+                commodity.priceChanged = false;
+                commodity.priceTitle = same;
+                commodity.tierDiffsChanged = [];
+                commodity.tierDiffs.forEach(function (){
+                    commodity.tierDiffsChanged.push(false);
+                })
+            });
+            this.signPrice = sames;
+        },
+        priceChange: function (commodity) {
+            commodity.priceChanged = commodity.price != commodity.reset;
+            commodity.priceTitle = commodity.priceChanged ? diff : same;
+            this.priceState();
+        },
+        diffChange: function (commodity, tier) {
+            commodity.tierDiffsChanged[tier] = commodity.tierDiffs[tier] != commodity.resetTierDiffs[tier];
+            commodity.priceTitle = commodity.tierDiffsChanged[tier] ? diff : same;
+            this.priceState();
+        },
+        dismissPricesReceived: function () {
+            $('#pricesReceived').removeClass('in');
+            this.updatePrices();
+        },
+        dismissPricesSent: function () {
+            $('#pricesSent').removeClass('in');
+            $('#pricesReceived').addClass('in');
+        },
+        displayPricesReceived: function (modal) {
+            if(modal) $('#'+modal).modal('toggle');
+            $('#pricesSent').addClass('in');
+        },
+        switchModal: function (current, next) {
+            $('#'+current).modal('toggle');
+            $('#'+next).modal('toggle');
+        },
+        priceState: function () {
+            let state = sames;
+            this.commodities.forEach(function (commodity) {
+                if(commodity.priceChanged) state = diffs;
+                commodity.tierDiffsChanged.forEach(function (tier) {
+                    if(tier) state = diffs;
+                })
+            });
+            this.signPrice = state;
+        },
+        updatePrices: function () {
+            this.commodities.forEach(function (commodity) {
+                commodity.reset = commodity.price;
+                commodity.resetTierDiffs.forEach(function (tier, index) {
+                    if(tier!=commodity.tierDiffs[index]){
+                        commodity.resetTierDiffs[index] = commodity.tierDiffs[index]
+                    }
+                })
+                commodity.priceChanged = false;
+                commodity.priceTitle = same;
+                commodity.tierDiffsChanged = [];
+                commodity.tierDiffs.forEach(function (){
+                    commodity.tierDiffsChanged.push(false);
+                })
+            });
+            this.priceState();
+        }
+    }
+});
